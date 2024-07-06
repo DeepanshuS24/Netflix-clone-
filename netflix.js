f450149708dd0a788706fc1141cba015db4d9950
Deepanshu <deepanshusharmaofficial7@gmail.com>
2024-07-06 18:26:15 +0530

    js

---
+++ netflix.js
@@ -0,0 +1,21 @@
+
+
+function questionclicked(){
+    document.addEventListener('click', () => {
+        const questions = document.querySelectorAll('.question');
+    
+        questions.forEach(question => {
+            question.addEventListener('click', () => {
+                const answer = question.nextElementSibling;
+    
+                if (answer.style.display === 'block') {
+                    answer.style.display = 'none';
+                } else {
+                    answer.style.display = 'block';
+                }
+            });
+        });
+    });
+
+}
+
